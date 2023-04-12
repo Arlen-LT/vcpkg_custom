adb783832e5743d3aba2fc1b56370013fdcaa8b7
arlen <arlen-lt@protonmail.com>
2023-04-12 10:00:32 +0000

    init repo with ports and triplets

---
+++ triplets/arm64-android.cmake
@@ -0,0 +1,7 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CMAKE_SYSTEM_NAME Android)
+set(VCPKG_BUILD_TYPE release)
+set(VCPKG_CMAKE_SYSTEM_VERSION 29)
+set(VCPKG_MAKE_BUILD_TRIPLET "--host=aarch64-linux-android --build=x86_64-linux-gnu")
---
+++ vcpkg_custom.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -e
+
+usage(){
+    echo "Usage: ${0##*/} [options]"
+    echo "--install <port_name>      install package"
+    echo "--version                  show version"
+    echo "--help                     show usage infomation"
+    exit 0
+}
+
+install(){
+
+    if [[ "$1" != "python3" ]]; then
+        echo "Not yet support this port"
+        exit 1;
+    fi
+
+    SOURCE_DIR=$PWD
+    PORT=python3
+    TRIPLET=arm64-android
+
+    # For vcpkg required
+    export ANDROID_NDK_HOME=$ANDROID_NDK
+
+    # For python3 cross-compile required
+    export READELF=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-readelf
+    export CONFIG_SITE=${SOURCE_DIR}/ports/${PORT}/config_site
+
+    # clone vcpkg and install
+    pushd $VCPKG_ROOT
+    ./vcpkg install ${PORT}:${TRIPLET} --overlay-ports=${SOURCE_DIR}/ports --overlay-triplets=${SOURCE_DIR}/triplets
+    popd
+}
+
+options=$(getopt -n "Installing VS Code Server" -l "help,version,install:" -o "hv" -a -- "$@")
+eval set -- ${options}
+while true
+do
+    case "$1" in
+    	--install) shift; install $1 ;;
+    	-h | --help) usage ;;
+    	--version) echo "${0##*/} worked with vcpkg:203383666e2422ed31e1faebe6efa6e306bd126d"; exit 0 ;;
+    	--) shift; break;;
+    esac
+    shift
+done
+
+
