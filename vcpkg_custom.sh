7889d337a8f2a5097a744992c24610c40eba5090
arlen <arlen-lt@protonmail.com>
2023-04-20 11:47:35 +0000

    install python3 site-packages

--- vcpkg_custom.sh
+++ vcpkg_custom.sh
@@ -1,5 +1,25 @@
 #!/bin/bash -eu
 
+export ANDROID_NDK_HOME=${ANDROID_NDK} # For vcpkg required
+export CC=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android29-clang
+export CXX=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android29-clang++
+export AR=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar
+export AS=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-as
+export LD=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/ld
+export LINK=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-link
+export NM=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-nm
+export OBJCOPY=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-objcopy
+export OBJDUMP=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-objdump
+export PROFDATA=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-profdata
+export RANLIB=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ranlib
+export READELF=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-readelf
+export STRIP=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-strip
+export YASM=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/yasm
+export SYSROOT=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot
+
+BUILD_TRIPLET=x64-linux
+HOST_TRIPLET=arm64-android
+
 usage(){
     echo "Usage: ${0##*/} [options]"
     echo "--install <port_name>      install package"
@@ -10,52 +30,53 @@ usage(){
 }
 
 install(){
-    SOURCE_DIR=$PWD
+    SOURCE_DIR=$(dirname $(realpath $0))
     PORT=$1
-    BUILD_TRIPLET=x64-linux
-    HOST_TRIPLET=arm64-android
 
-    # For vcpkg required
-    export ANDROID_NDK_HOME=$ANDROID_NDK
+    # Must install BUILD_TRIPLET first
+    /bin/bash -c "${VCPKG_ROOT}/vcpkg install ${PORT}:${BUILD_TRIPLET}"
 
-    # clone vcpkg and install
-    pushd $VCPKG_ROOT
-    ./vcpkg install ${PORT}:${BUILD_TRIPLET}
+    # use overlay ports & triplets
+    ${VCPKG_ROOT}/vcpkg install ${PORT}:${HOST_TRIPLET} --overlay-ports=${SOURCE_DIR}/ports --overlay-triplets=${SOURCE_DIR}/triplets --debug-env
 
-    prerequisite_env
-    ./vcpkg install ${PORT}:${HOST_TRIPLET} --overlay-ports=${SOURCE_DIR}/ports --overlay-triplets=${SOURCE_DIR}/triplets --debug-env
-    popd
+    if [[ "$1" == "python3" ]]; then
+        handle_python3_artifact ${SOURCE_DIR}/installed/${PORT}
+    fi
 }
 
 remove(){
-    PORT=$1
-    HOST_TRIPLET=arm64-android
-
-    pushd $VCPKG_ROOT
-    ./vcpkg remove ${PORT}:${HOST_TRIPLET}
-    popd
+    ${VCPKG_ROOT}/vcpkg remove $1:${HOST_TRIPLET}
 }
 
-prerequisite_env(){
-    # For python3 cross-compile required
-    export CC=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android29-clang
-    export CXX=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android29-clang++
-    export AR=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar
-    export AS=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-as
-    export LD=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/ld
-    export LINK=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-link
-    export NM=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-nm
-    export OBJCOPY=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-objcopy
-    export OBJDUMP=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-objdump
-    export PROFDATA=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-profdata
-    export RANLIB=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ranlib
-    export READELF=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-readelf
-    export STRIP=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-strip
-    export YASM=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/yasm
-    #patchelf --set-soname libpython3.10.so $VCPKG_ROOT/installed/arm64-android/lib/libpython3.10.so
+handle_python3_artifact(){
+    PYTHON3_ARITFACT_DIR=$1
+    CROSSENV_DIR=${PYTHON3_ARITFACT_DIR}/../venv
+
+    if [ -d ${PYTHON3_ARITFACT_DIR} ]; then
+        rm -rf ${PYTHON3_ARITFACT_DIR}
+    fi
+
+    if [ -d ${CROSSENV_DIR} ]; then
+        rm -rf ${CROSSENV_DIR}
+    fi
+
+    # fake a common installed python directory structure to let crossenv know where to find include headers and libraries.
+    cp -R ${VCPKG_ROOT}/packages/python3_${HOST_TRIPLET} ${PYTHON3_ARITFACT_DIR} \
+        && mv ${PYTHON3_ARITFACT_DIR}/tools/python3 ${PYTHON3_ARITFACT_DIR}/bin
+
+    SYSCONFIG=${PYTHON3_ARITFACT_DIR}/lib/python3.10/_sysconfigdata__linux_aarch64-linux-android.py
+    sed -i -e '2,3d' -e "1a _base = \'${PYTHON3_ARITFACT_DIR}\'" ${SYSCONFIG}
+    
+    pyenv global 3.10.7
+    pip install crossenv
+    python3 -m crossenv --sysconfigdata-file=${SYSCONFIG} \
+        --sysroot=${SYSROOT} --env LDFLAGS="-lpython3.10" ${PYTHON3_ARITFACT_DIR}/bin/python3.10 ${CROSSENV_DIR}
+    . ${CROSSENV_DIR}/bin/activate
+    cross-pip install yt-dlp
+    cp -r ${CROSSENV_DIR}/cross/lib/python3.10/site-packages/* ${PYTHON3_ARITFACT_DIR}/lib/python3.10/site-packages/
 }
 
-options=$(getopt -n "vcpkg_custom" -l "help,version,install:,remove:" -o "hv" -a -- "$@")
+options=$(getopt -n "${0##*/}" -l "help,version,install:,remove:" -o "h" -a -- "$@")
 eval set -- ${options}
 while true
 do
