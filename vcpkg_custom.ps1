b5338bc48dc47a0aab1ff22d198b7ed8979d5350
Arlen-LT <arlen-lt@protonmail.com>
2023-05-25 17:59:56 +0800

    support windows x64 python3

---
+++ vcpkg_custom.ps1
@@ -0,0 +1,8 @@
+& $env:VCPKG_ROOT/vcpkg install python3:x64-windows
+
+$PYTHON3_ARITFACT_DIR="$env:VCPKG_ROOT/installed/x64-windows/tools/python3"
+& ${PYTHON3_ARITFACT_DIR}/python --version
+& ${PYTHON3_ARITFACT_DIR}/python -m ensurepip
+& ${PYTHON3_ARITFACT_DIR}/python -m pip install yt_dlp
+Get-ChildItem -Path ${PYTHON3_ARITFACT_DIR} -include "__pycache__","*.py[co]" -Recurse | rm -r -Force
+7z a python3_windows.zip ${PYTHON3_ARITFACT_DIR}/*
